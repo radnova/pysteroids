e7104e09bc108b59672a5aac5a4dc61323e4e7f6
Rob Weeks <weeks@radiantnova.com>
2024-08-27 17:26:44 -0400

    busted

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    run = True
+    frames = pygame.time.Clock()
+    dt = 0
     
-    while True:
-        pygame.Surface.fill(screen,(0,0,0,))
+    player = PlayerShape(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
+    while True:
+        pygame.Surface.fill(screen,(0,0,0))
+        player.draw(screen)
+        player.update(dt)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
@@ -17,6 +23,7 @@ def main():
         # print(f"Screen width: {SCREEN_WIDTH}")
         # print(f"Screen height: {SCREEN_HEIGHT}")
         pygame.display.flip()
+        dt = frames.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
