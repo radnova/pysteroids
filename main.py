df86a651a9dbd5235e93b1c5e45f7712193dc8af
Rob Weeks <weeks@radiantnova.com>
2024-08-27 17:47:44 -0400

    starting over

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
 import pygame
 from constants import *
-from player import *
-from circleshape import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    frames = pygame.time.Clock()
-    dt = 0
-    
-    player = PlayerShape(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
     while True:
-        pygame.Surface.fill(screen,(0,0,0))
-        player.draw(screen)
-        player.update(dt)
+        screen.fill("Black")
+        pygame.display.flip()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return 
-
-        # print("Starting asteroids!")
-        # print(f"Screen width: {SCREEN_WIDTH}")
-        # print(f"Screen height: {SCREEN_HEIGHT}")
-        pygame.display.flip()
-        dt = frames.tick(60) / 1000
+                return
 
 if __name__ == "__main__":
     main()
