735b765c30527d1b5f276b76770609b106ec5d35
Rob Weeks <weeks@radiantnova.com>
2024-08-27 19:23:05 -0400

    player now draws and rotates on screen

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
-        screen.fill("Black")
-        pygame.display.flip()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        screen.fill("Black")
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
 
 if __name__ == "__main__":
     main()
