2c69f452834f1555ddaed86a00d225c7ffe6fc77
Rob Weeks <weeks@radiantnova.com>
2024-08-28 20:03:08 -0400

    added collission game over

--- main.py
+++ main.py
@@ -1,37 +1,47 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
-
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    roids = pygame.sprite.Group()
 
-    updatable.add(player)
-    drawable.add(player)
+    Asteroid.containers = (roids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Player.containers = (updatable, drawable)
+    
+    field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+         
+        for sprite in updatable:
+            sprite.update(dt)   
+            
+        for roid in roids:
+            if player.collide(roid):
+                print("Game over!")
+                return
             
         screen.fill("Black")
-        for sprite in updatable:
-            sprite.update(dt)
+        
         for sprite in drawable:
             sprite.draw(screen)
-        #player.update(dt)
-        #player.draw(screen)
+
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
-
-
 
 if __name__ == "__main__":
     main()
