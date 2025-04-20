8f6e68d72b3c7e4e7d8994db27ce81b52d1ea400
Rob Weeks <weeks@radiantnova.com>
2025-04-20 16:22:25 -0400

    CH2 L1 - CH3 L6

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from shot import *
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 def main2():
     pygame.init()
